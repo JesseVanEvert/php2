c957b9518f7f7105097533b2063a49fba019b4f7
Jesse <625868@student.inholland.nl>
2022-11-13 13:48:48 +0100

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,4 @@
 <?php
-  // DB Constants params
-  //define('DB_HOST', 'db.nazaragency.nl');
-  //define('DB_USER', 'md500698db492431');
-  //define('DB_PASS', 'Phpiskut1!');
-  //define('DB_NAME', 'md500698db492431');
-
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
   define('DB_PASS', '');
